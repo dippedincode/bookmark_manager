a4c51bcb2942d97dcefec3507e2cd78b011948cf
AdrienFabre <adrien.fabre.1@gmail.com>
2019-01-28 16:41:42 +0000

    refactored bookmarks view

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require 'sinatra/base'
 class BookmarkManager < Sinatra::Base
 
   get '/' do
-
     erb :index
-
   end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end
+
 
   run! if app_file == $0
 
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark
 
-
-end 
+  def self.all
+    [
+      "http://www.makersacademy.com",
+      "http://www.destroyallsoftware.com",
+      "http://www.google.com"
+     ]
+   end
+end
